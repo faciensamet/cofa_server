b2eff1d83d8e8d3fa26a695c4387f912511aa9a0
skyzerotiger <82987097+faciensamet@users.noreply.github.com>
2021-04-29 00:16:12 +0900

    add config,

--- cofa_server.js
+++ cofa_server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
-var serverName = "Cofa #" + GetRandomInt(100, 999);
+var config = 
+{
+    serverName:"Cofa #" + GetRandomInt(100000, 999999),
+  
+    open_server: 1, // 0 - private, 1 - open(anyone can see your server)
+    permission_write: 0, // 0 - all, 1 - authorized person
+};
 
 function GetRandomInt(min, max) {
     min = Math.ceil(min);
@@ -11,20 +17,26 @@ function GetRandomInt(min, max) {
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db');
-// user : uuid 
-// content : base64
+
+// create db table  user = uuid,  content = base64
 db.run('CREATE TABLE if not exists "data" (	"no"	INTEGER,	"user"	TEXT, "content"	TEXT,	"date"	TEXT DEFAULT CURRENT_TIMESTAMP,	PRIMARY KEY("no" AUTOINCREMENT));');
 
 exports.Start = () => {
     app.listen(3000)
     app.get('/', function (req, res) 
     {
-        res.send('Hello World - ' + serverName);
+        res.send('Hello World - ' + config.serverName);
     });
 
-    app.get("/v1/chat", function(req, res)
+    app.get("/v1/add", function(req, res)
     {
         db.run('INSERT INTO "main"."data"("user", "content") VALUES ("' + req.query.u + '", "' + req.query.c + '");');
+        res.send("1");
+    });
+
+    app.get("/v1/get", function(req, res)
+    {
+        
         res.send("1");
     });
 }
