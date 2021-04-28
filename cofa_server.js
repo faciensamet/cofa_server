ff95a3d40638def4434298a171affeef57831642
skyzerotiger <82987097+faciensamet@users.noreply.github.com>
2021-04-28 23:45:57 +0900

    add sqlite,

--- cofa_server.js
+++ cofa_server.js
@@ -9,9 +9,19 @@ function GetRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
 }
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db');
+db.run('CREATE TABLE if not exists "data" (	"no"	INTEGER,	"content"	TEXT,	"date"	TEXT DEFAULT CURRENT_TIMESTAMP,	PRIMARY KEY("no" AUTOINCREMENT));');
+
 exports.Start = () => {
     app.listen(3000)
-    app.get('/', function (req, res) {
+    app.get('/', function (req, res) 
+    {
         res.send('Hello World - ' + serverName);
-    })
+    });
+
+    app.get("/v1/chat", function(req, res)
+    {        
+        res.send("1");
+    });
 }
