05cad52b632c9f43b3fbd60174ff55949e75ae7f
skyzerotiger <82987097+faciensamet@users.noreply.github.com>
2021-04-29 00:07:09 +0900

    chat api

--- cofa_server.js
+++ cofa_server.js
@@ -6,12 +6,14 @@ var serverName = "Cofa #" + GetRandomInt(100, 999);
 function GetRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+    return Math.floor(Math.random() * (max - min)) + min; 
 }
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db');
-db.run('CREATE TABLE if not exists "data" (	"no"	INTEGER,	"content"	TEXT,	"date"	TEXT DEFAULT CURRENT_TIMESTAMP,	PRIMARY KEY("no" AUTOINCREMENT));');
+// user : uuid 
+// content : base64
+db.run('CREATE TABLE if not exists "data" (	"no"	INTEGER,	"user"	TEXT, "content"	TEXT,	"date"	TEXT DEFAULT CURRENT_TIMESTAMP,	PRIMARY KEY("no" AUTOINCREMENT));');
 
 exports.Start = () => {
     app.listen(3000)
@@ -21,7 +23,8 @@ exports.Start = () => {
     });
 
     app.get("/v1/chat", function(req, res)
-    {        
+    {
+        db.run('INSERT INTO "main"."data"("user", "content") VALUES ("' + req.query.u + '", "' + req.query.c + '");');
         res.send("1");
     });
 }
